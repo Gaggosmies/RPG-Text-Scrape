e3902f1e4bf2e146fc10a266d57d6d8c41a76e41
Gaggosmies <Gaggosmies@hotmail.com>
2023-01-21 18:38:49 +0200

    Items are now written into an output file, which is identical to RPG maker's

--- Items.cs
+++ Items.cs
@@ -1,28 +1,60 @@
 ﻿using Newtonsoft.Json.Linq;
-using RPGTextToPlugin.jsonClasses;
-using System.Text.Json.Serialization;
 
 namespace RPGTextToPlugin
 {
     internal class Items
     {
-        public static bool DoItemsStuff(Common.Globals globals)
+        // gets and parses stuff in items.json. If file not found, FileNotFoundException is thrown
+        public static JArray GetItemsJsonArray()
         {
+            // todo: fix file location?
             string fileLocationItemsJson = Common.debugLocation(1);
-            JArray itemJsonArray;
 
             // check if file is actually found
             if (File.Exists(fileLocationItemsJson))
             {
                 string jsonString = System.IO.File.ReadAllText(fileLocationItemsJson);
-                itemJsonArray = JArray.Parse(jsonString);
+                return JArray.Parse(jsonString);
             }
             else
             {
-                return false;
+                // todo: error handling
+                throw new FileNotFoundException("Custom Error Message: Items.json not found at " + Common.debugLocation(1));
+            }
+        }
+
+        public static bool DoItemsStuff(Common.Globals globals)
+        {
+            JArray itemJsonArray = new JArray();
+            string outputLocation = System.AppDomain.CurrentDomain.BaseDirectory + "gottenItems.json";
+
+            itemJsonArray = GetItemsJsonArray();
+
+            // write output into a file using RPG maker format
+            using (StreamWriter sw = File.CreateText(outputLocation))
+            {
+                // Wrap in json array, first json object always null in RPG maker
+                sw.WriteLine("[\nnull,");
+
+                int i = 0;
+                foreach (JObject jObject in itemJsonArray.Children<JObject>())
+                {
+                    i++;
+                    sw.Write(jObject.ToString(Newtonsoft.Json.Formatting.None));
+                    
+                    if (i != itemJsonArray.Count - 1)
+                    {
+                        sw.WriteLine(",");
+                    }
+                }
+
+                sw.Write("\n]");
             }
 
 
+            Console.WriteLine($"Surely there's a lot happenin in {outputLocation}...");
+            Console.WriteLine("\n\nPress the any key to continue...");
+            System.Console.ReadKey();
 
             return false;
         }
